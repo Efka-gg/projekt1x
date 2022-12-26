d2f8e7b9409c8452b55658570b12c1e62c06be99
Efka-gg <121442341+Efka-gg@users.noreply.github.com>
2022-12-26 16:12:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    eboticsMIBO.forward()
+})
+input.onButtonPressed(Button.B, function () {
+    eboticsMIBO.back()
+})
+eboticsMIBO.init_wheel(AnalogPin.P1, AnalogPin.P2)
